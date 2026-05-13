4a84c5a2d621ac8e485e6354af9b8a1e0950d537
SG\devanshuk <devanshuk@SteepGraph.com>
2026-05-13 18:46:25 +0530

    post request

--- server.js
+++ server.js
@@ -3,14 +3,37 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
-
-app.get('/HELLO', (req, res) => {
-  res.send('Hello World')
-})
-
 const PORT = process.env.PORT || 3000
 
+// Enable CORS
+app.use(cors())
+
+// Parse JSON body
+app.use(express.json())
+
+// Simple GET Service
+app.get('/HELLO', (req, res) => {
+
+  res.send('Hello World')
+
+})
+
+// Simple POST Service
+app.post('/submit', (req, res) => {
+
+  console.log('Received Data:', req.body)
+
+  res.json({
+    success: true,
+    message: 'POST request received successfully',
+    receivedData: req.body
+  })
+
+})
+
+// Start Server
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
+
+  console.log(`Server running on port ${PORT}`)
+
 })
